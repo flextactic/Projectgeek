30687981d685e41a2d17a862f6af4085dd2a306e
anshulsharma123 <anshul.sharma2019@vitbhopal.ac.in>
2021-05-24 23:30:29 +0530

    added all depecdencies and solved server crash

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const user = require('../routes/user')
+const user = require('../routes/users')
 
 
 module.exports = function(app)
 {
    app.use(express.json());
    app.use(express.static("public"));
-   app.use('./api/user',user);
+  // app.use('./api/user',user);
 }
