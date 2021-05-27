054c2b1992f74c717f51b8a4842e8ce2d9eb907c
Krishan-2000 <krishan.mishra2019@vitbhopal.ac.in>
2021-05-27 23:36:40 +0530

    Add UsersProject Database Management post put get delete

---
+++ routes/user_projects.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const router = new express.Router();
+const Project= require("../models/user_project");
+const bcrypt = require('bcryptjs')
+const _ = require('lodash')
+const auth =require('../middlewares/auth')
+const mongoose =require('mongoose')
+const validateProject= require("../models/user_project");
+const validateEditProject= require("../models/user_project");
+
+// TODO: "CREATE PROJECT API";
+router.post("/create_project", async(req, res)=>{
+       
+        const { error } = validateProject(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+    try{
+        const project= new Project(req.body);
+        const createProject = await project.save();
+        res.status(201).send(createProject);
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
+})
+
+// TODO: GET PROJECTS API
+
+router.get("/get_projects",async(req,res)=>{
+    try{
+        const projectData = await Project.find().populate('User');
+        res.status(200).send(projectData);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+//TODO GET SINGLE PROJECT API
+
+router.get("/get_project/:id", async(req,res)=>{
+    try{
+        const _id= req.params.id; 
+        const projectData= await Project.findById(_id).populate("User");
+
+        if(!projectData){
+            return res.status(404).send();
+        }else
+        {
+            res.send(projectData);
+        }
+    }catch(e){
+       res.status(400).send(e); 
+    }
+})
+
+// TODO Update the project detail by it id
+
+router.put("/update_project/:id", async(req,res)=>{
+
+    const { error } = validateEditProject(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try{
+        const _id= req.params.id;
+        if(!_id) return res.status(400).send();
+
+        const updateProject= await Project.findByIdAndUpdate(_id,req.body,{new :true});
+        console.log(updateProject);
+        res.status(200).send(updateProject);
+    }catch(e)
+    {
+        res.status(401).send("Error!... Please try again later..");
+    }
+   
+})
+
+// TODO: Delete particular project of a user
+
+router.delete("/delete_project/:id",async(req,res)=>{
+    try{
+        const deleteProject= await Project.findByIdAndDelete(req.params.id);
+        if(!req.params.id){
+            return res.status(400).send();
+        }
+        res.send(deleteProject);
+    }
+    catch(e){
+        res.status(500).send("Opps... Please try again later");
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('./startup/db')();
 //routes 
 require("./startup/routes")(app);
 
-const port=process.env.port||3000;
+const port=process.env.port||7000;
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
 });
