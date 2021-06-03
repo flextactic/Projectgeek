992ed868def98e3833ce6dbe61639aef4d25ef0b
Drishtdyumnss <78607331+Drishtdyumnss@users.noreply.github.com>
2021-06-03 10:44:54 +0530

    navbar added

---
+++ client/src/Components/Userepos.js
@@ -0,0 +1,12 @@
+import  {React, Fragment } from 'react';
+import './Userepos.css';
+
+export const Homepage = () => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default Homepage;
--- client/src/Components/profilecard.js
+++ client/src/Components/profilecard.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import './Homepage.css';
+import './profilecard.css';
+
 const homepage = (props) => {
     
     return(
         <div className="container">
             <div className="row">
-            <div className="card col">
                 <div className="content">
                     <p className="name"><h1>{props.name+"  "}</h1></p>
                     <div className="cropper"><img className="img" src={props.avatar} alt="loading"/></div>
                     <p className="id">profileid:{props.usergithub+" "}</p>
                 </div>
             </div>
-            </div>
-            <div className="card-container">
-                <div className="row align-items-end">
-                    <div className="card-project col">
-
-                    </div>
-                </div>
-            </div>
         </div>
+        
     );
 }
 
